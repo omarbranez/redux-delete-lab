311dc7ed0ec62c486421fe2f4ecdd4ef02bd6c67
Omar <omar.branez@gmail.com>
2021-10-14 02:28:50 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,6 +3,7 @@ import Band from "./Band"
 
 const Bands = props => {
     const bands = props.bands.map(band => <Band key={band.id} deleteBand={props.deleteBand} {...band} />)
+    // {...band} adds both ID={BAND.ID} and name={BAND.NAME}
         return(
             <div>
                 {bands}
